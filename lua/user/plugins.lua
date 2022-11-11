bdf664ed17228e4637ddafc54f5f3c34d9c3b768
Rogerskelamen <rogerskelamen@gmail.com>
2022-11-11 22:52:49 +0800

    add colorizer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,7 +56,6 @@ return packer.startup(function(use)
       run = function() vim.fn["mkdp#util#install"]() end,
   }
 
-
   -- Colorschemes
   use "sainnhe/everforest"
   use { "catppuccin/nvim", as = "catppuccin" }
@@ -122,6 +121,40 @@ return packer.startup(function(use)
 
   -- Build-in ranger
   use "kevinhwang91/rnvimr"
+
+  -- Chinese Vim help document
+  use "yianwillis/vimcdoc"
+
+  -- Colorizer
+  use {
+    'NvChad/nvim-colorizer.lua',
+    config = function ()
+      require "colorizer".setup {
+        filetypes = { "*" },
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = true, -- "Name" codes like Blue or blue
+          RRGGBBAA = false, -- #RRGGBBAA hex codes
+          AARRGGBB = false, -- 0xAARRGGBB hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = false, -- CSS hsl() and hsla() functions
+          css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          -- Available modes for `mode`: foreground, background,  virtualtext
+          mode = "virtualtext", -- Set the display mode.
+          -- Available methods are false / true / "normal" / "lsp" / "both"
+          -- True is same as normal
+          tailwind = false, -- Enable tailwind colors
+          -- parsers can contain values used in |user_default_options|
+          sass = { enable = false, parsers = { css }, }, -- Enable sass colors
+          virtualtext = "■",
+        },
+        -- all the sub-options of filetypes apply to buftypes
+        buftypes = {},
+      }
+    end
+  }
 
   -- Treesitter
   use {
