4d71618d6e42f0b8d5e72006fbda208472ed8f98
Rogerskelamen <rogerskelamen@gmail.com>
2022-11-12 11:04:37 +0800

    config colorizer

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -126,35 +126,7 @@ return packer.startup(function(use)
   use "yianwillis/vimcdoc"
 
   -- Colorizer
-  use {
-    'NvChad/nvim-colorizer.lua',
-    config = function ()
-      require "colorizer".setup {
-        filetypes = { "*" },
-        user_default_options = {
-          RGB = true, -- #RGB hex codes
-          RRGGBB = true, -- #RRGGBB hex codes
-          names = true, -- "Name" codes like Blue or blue
-          RRGGBBAA = false, -- #RRGGBBAA hex codes
-          AARRGGBB = false, -- 0xAARRGGBB hex codes
-          rgb_fn = true, -- CSS rgb() and rgba() functions
-          hsl_fn = false, -- CSS hsl() and hsla() functions
-          css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-          css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-          -- Available modes for `mode`: foreground, background,  virtualtext
-          mode = "virtualtext", -- Set the display mode.
-          -- Available methods are false / true / "normal" / "lsp" / "both"
-          -- True is same as normal
-          tailwind = false, -- Enable tailwind colors
-          -- parsers can contain values used in |user_default_options|
-          sass = { enable = false, parsers = { css }, }, -- Enable sass colors
-          virtualtext = "■",
-        },
-        -- all the sub-options of filetypes apply to buftypes
-        buftypes = {},
-      }
-    end
-  }
+  use "NvChad/nvim-colorizer.lua"
 
   -- Treesitter
   use {
