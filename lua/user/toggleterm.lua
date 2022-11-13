7b37dff576e880029de5320c58dbbb199d5dd497
Rogerskelamen <rogerskelamen@gmail.com>
2022-11-13 19:42:46 +0800

    debug

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -36,4 +36,4 @@ function _G.set_terminal_keymaps()
   vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
 end
 
-vim.cmd('autocmd TermOpen term://* lua set_terminal_keymaps()')
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
