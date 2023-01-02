3b4ef476bfaadd5af73724742f3a261d8fb79dfb
Rogerskelamen <rogerskelamen@gmail.com>
2023-01-02 21:36:46 +0800

    change colorscheme format

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -12,26 +12,26 @@ g.everforest_better_performance = 1
 
 -- catppuccin
 require("catppuccin").setup({
-    flavour = "frappe", -- latte, frappe, macchiato, mocha
-    background = { -- :h background
-        light = "latte",
-        dark = "frappe",
-    },
-    -- compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
-    transparent_background = true,
-    term_colors = true,
-    styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-    },
-    integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-    },
+  flavour = "frappe", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = "latte",
+    dark = "frappe",
+  },
+  -- compile_path = vim.fn.stdpath("cache") .. "/catppuccin",
+  transparent_background = true,
+  term_colors = true,
+  styles = {
+    comments = { "italic" },
+    conditionals = { "italic" },
+  },
+  integrations = {
+    cmp = true,
+    gitsigns = true,
+    nvimtree = true,
+    telescope = true,
+    treesitter = true,
+    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+  },
 });
 
 -- tokyonight --
