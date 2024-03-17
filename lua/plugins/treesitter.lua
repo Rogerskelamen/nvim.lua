b95590e825c27611fad938c2659cd95b78e35cf9
Rogerskelamen <rogerkelamen@gmail.com>
2024-03-17 21:02:33 +0800

    change treesitter back

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,58 +4,65 @@ return {
   event = { "VeryLazy" },
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "vimdoc",
-      "markdown",
-      "c",
-      "cpp",
-      "make",
-      "python",
-      "html",
-      "css",
-      "javascript",
-      "json",
-      "vue",
-      "scala",
-      "go",
-      "dart",
-      "yaml",
-      "xml",
-    },
-    sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = {
-      enable = true,
-      disable = {
+  config = function()
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
+
+    configs.setup {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "c",
+        "cpp",
+        "make",
+        "python",
         "html",
         "css",
-        "yaml",
-      }
-    },
-    autotag = {
-      enable = true,
-      filetypes = {
-        "html",
-        "xml",
         "javascript",
-        "typescript",
+        "json",
         "vue",
-        "jsx",
-        "tsx",
-        "javascriptreact",
-        "typescriptreact",
-        "rescript",
-        "php",
-        "markdown",
+        "scala",
+        "go",
+        "dart",
+        "yaml",
+        "xml",
       },
-    },
-  }
+      sync_install = false,
+      ignore_install = { "" }, -- List of parsers to ignore installing
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+      },
+      indent = {
+        enable = true,
+        disable = {
+          "html",
+          "css",
+          "yaml",
+        }
+      },
+      autotag = {
+        enable = true,
+        filetypes = {
+          "html",
+          "xml",
+          "javascript",
+          "typescript",
+          "vue",
+          "jsx",
+          "tsx",
+          "javascriptreact",
+          "typescriptreact",
+          "rescript",
+          "php",
+          "markdown",
+        },
+      },
+    };
+  end
 }
