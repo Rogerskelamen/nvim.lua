56591b315106b0ccca71f5404efff5e51ecbf5ef
Rogerskelamen <rogerskelamen@gmail.com>
2024-07-28 20:37:03 +0800

    add flex to comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,26 +3,38 @@ return {
   event = "VeryLazy",
 
   opts = {
-    toggler = {
-      ---Line-comment toggle keymap
-      line = '<leader>c<leader>',
-      ---Block-comment toggle keymap
-      block = '<leader>cb',
-    },
-    opleader = {
-      ---Line-comment keymap
-      line = '<leader>c<leader>',
-      ---Block-comment keymap
-      block = '<leader>cb',
-    },
-    extra = {
-      ---Add comment on the line above
-      above = '<leader>cO',
-      ---Add comment on the line below
-      below = '<leader>co',
-      ---Add comment at the end of line
-      eol = '<leader>ca',
-    },
-  }
-}
+  },
 
+  config = function()
+    local status_ok, comment = pcall(require, 'Comment')
+    if not status_ok then
+      return
+    end
+
+    comment.setup {
+      toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>c<leader>',
+        ---Block-comment toggle keymap
+        block = '<leader>cb',
+      },
+      opleader = {
+        ---Line-comment keymap
+        line = '<leader>c<leader>',
+        ---Block-comment keymap
+        block = '<leader>cb',
+      },
+      extra = {
+        ---Add comment on the line above
+        above = '<leader>cO',
+        ---Add comment on the line below.
+        below = '<leader>co',
+        ---Add comment at the end of line
+        eol = '<leader>ca',
+      },
+    }
+
+    local ft = require('Comment.ft')
+    ft.lex = {'/*%s*/'}
+  end,
+}
