e31d4a5d9cc42acdb4d1f8e2f3df7ca271c073b5
Rogerskelamen <rogerskelamen@gmail.com>
2024-10-30 19:14:43 +0800

    change markdown-preview config

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
   "iamcco/markdown-preview.nvim",
-  build = function() vim.fn["mkdp#util#install"]() end,
+  build = "cd app && npm install",
   dependencies = "dhruvasagar/vim-table-mode",
 
   config = function()
