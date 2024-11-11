ded359e7aeb7b82a9397f9590e052933818ff560
Rogerskelamen <rogerskelamen@gmail.com>
2024-11-11 23:33:24 +0800

    embed yazi to neovim(win version)

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -60,5 +60,26 @@ return {
     config = function()
       require "impatient".enable_profile()
     end
+  },
+
+  -- Yazi Embedding
+  {
+    "mikavilpas/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>r",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+    },
+    opts = {
+      -- if you want to open yazi instead of netrw, see below for more info
+      open_for_directories = false,
+      keymaps = {
+        show_help = '<f1>',
+      },
+    },
   }
 }
