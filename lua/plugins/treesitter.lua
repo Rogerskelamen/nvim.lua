e131c7e646ccb6f47b59012265ff3d70c5a66c7d
Rogerskelamen <rogerskelamen@gmail.com>
2025-01-25 22:33:01 +0800

    change back to treesitter default config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,43 +4,51 @@ return {
   event = { "VeryLazy" },
   cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "vimdoc",
-      "markdown",
-      "c",
-      "cpp",
-      "make",
-      "cmake",
-      "python",
-      "html",
-      "css",
-      "javascript",
-      "json",
-      "vue",
-      "scala",
-      "go",
-      "dart",
-      "yaml",
-      "xml",
-    },
-    sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = {
-      enable = true,
-      disable = {
+  config = function()
+    local status_ok, configs = pcall(require, "nvim-treesitter.configs")
+    if not status_ok then
+      return
+    end
+
+    configs.setup {
+      ensure_installed = {
+        "lua",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "c",
+        "cpp",
+        "make",
+        "cmake",
+        "python",
         "html",
         "css",
-        "xml",
+        "javascript",
+        "json",
+        "vue",
+        "scala",
+        "go",
+        "dart",
         "yaml",
-      }
-    },
-  };
+        "xml",
+      },
+      sync_install = false,
+      ignore_install = { "" }, -- List of parsers to ignore installing
+      highlight = {
+        enable = true, -- false will disable the whole extension
+        disable = { "" }, -- list of language that will be disabled
+        additional_vim_regex_highlighting = true,
+      },
+      indent = {
+        enable = true,
+        disable = {
+          "html",
+          "css",
+          "xml",
+          "yaml",
+        }
+      },
+    };
+  end
 }
+
