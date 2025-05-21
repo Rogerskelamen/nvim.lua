e3e15ecfbe687d43b4acea8eb6ae833cea895b38
Rogerskelamen <rogerskelamen@gmail.com>
2025-05-21 12:58:24 +0800

    feat: add powershell support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -45,6 +45,18 @@ local options = {
   foldcolumn = '0',                        -- don't create a sign column for fold indicator
   foldmethod = "expr",                     -- define fold method in other way
   foldexpr = "nvim_treesitter#foldexpr()", -- use tree-sitter to define how to fold
+  --[[ use windows powershell instead of cmd.exe
+       see 'h: shell-powershell' for details ]]
+  shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+  shellcmdflag = [[-NoLogo -NonInteractive -ExecutionPolicy RemoteSigned -Command
+    [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();
+    $PSDefaultParameterValues['Out-File:Encoding']='utf8';
+    $PSStyle.OutputRendering='plaintext';
+    Remove-Alias -Force -ErrorAction SilentlyContinue tee;
+  ]],
+  shellredir = [[ 2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode ]],
+  shellpipe  = [[ 2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode ]],
+  shellxquote = "",
 }
 
 vim.opt.shortmess:append "c"
