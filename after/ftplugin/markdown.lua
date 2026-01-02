b1e648df1330249f55a488eebc1c5c58a78e7eb8
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-02 21:08:06 +0800

    fix: change keymap in markdown/dart with exact buffer

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 vim.opt_local.tabstop = 4 -- 4 spaces for a indent
 vim.opt_local.shiftwidth = 4
 
-local opts = { noremap = true, silent = true }
+local opts = { noremap = true, silent = true, buffer = true }
 vim.keymap.set("n", ",b", "a****<Esc>hi", opts)
 vim.keymap.set("n", ",i", "a**<Esc>i", opts)
 vim.keymap.set("n", ",u", "a<u></u><Esc>3hi", opts)
