f621e4f8febe3f1a13d9a30464cb20502feecdb5
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-25 18:11:01 +0800

    feat: add treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,15 @@ return {
 
     opts = {
       highlight = { enable = true },
-      indent = { enable = true },
+      indent = {
+        enable = true,
+        disable = {
+          "html",
+          "css",
+          "xml",
+          "yaml",
+        }
+      },
       folds = { enable = true },
       ensure_installed = {
         "lua",
@@ -47,6 +55,7 @@ return {
 
       vim.api.nvim_create_autocmd("FileType", {
         desc = "Enable Treesitter",
+        group = vim.api.nvim_create_augroup("treesitter", { clear = true }),
         callback = function(event)
           local lang = vim.treesitter.language.get_lang(event.match)
           local bufnr = event.buf
@@ -74,6 +83,7 @@ return {
           if enabled("folds") then
             vim.wo.foldmethod = "expr"
             vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+            vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
           end
         end,
       })
@@ -81,7 +91,111 @@ return {
   },
 
   {
-    
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    event = "VeryLazy",
+    opts = {
+      select = {
+        enable = true,
+        lookahead = true,
+        keys = {
+          ["af"] = "@function.outer",
+          ["if"] = "@function.inner",
+          ["ac"] = "@class.outer",
+          ["ic"] = "@class.inner",
+          ["aa"] = "@parameter.outer",
+          ["ia"] = "@parameter.inner",
+          -- You can also use captures from other query groups like `locals.scm`
+          ["as"] = { query = "@local.scope", query_group = "locals"},
+        },
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V', -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        -- extention to create buffer-local keymaps
+        keys = {
+          goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer", ["]a"] = "@parameter.inner" },
+          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+          goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer", ["[a"] = "@parameter.inner" },
+          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer", ["[A"] = "@parameter.inner" },
+        },
+      },
+    },
+    config = function(_, opts)
+      local TS = require("nvim-treesitter-textobjects")
+      TS.setup(opts)
+
+      local function attach(buf)
+        local ft = vim.bo[buf].filetype
+        local installed = require("nvim-treesitter").get_installed()
+        local lang = vim.treesitter.language.get_lang(ft)
+
+        if not (vim.tbl_get(opts, "move", "enable") and vim.tbl_contains(installed, lang)) then
+          return
+        end
+
+        local select = vim.tbl_get(opts, "select", "keys") or {}
+        ---@type table<string, table<string, string>>
+        local moves = vim.tbl_get(opts, "move", "keys") or {}
+
+        for key, query in pairs(select) do
+          local query_group = "textobjects"
+          if type(query) == "table" then
+            query = query.query
+            query_group = query.query_group
+          end
+          vim.keymap.set({ "x", "o" }, key, function()
+            require("nvim-treesitter-textobjects.select").select_textobject(query, query_group)
+          end, {
+            buffer = buf,
+            silent = true,
+          })
+        end
+
+        for method, keymaps in pairs(moves) do
+          for key, query in pairs(keymaps) do
+            local queries = type(query) == "table" and query or { query }
+            local parts = {}
+            for _, q in ipairs(queries) do
+              local part = q:gsub("@", ""):gsub("%..*", "")
+              part = part:sub(1, 1):upper() .. part:sub(2)
+              table.insert(parts, part)
+            end
+            local desc = table.concat(parts, " or ")
+            desc = (key:sub(1, 1) == "[" and "Prev " or "Next ") .. desc
+            desc = desc .. (key:sub(2, 2) == key:sub(2, 2):upper() and " End" or " Start")
+            if not (vim.wo.diff and key:find("[cC]")) then
+              vim.keymap.set({ "n", "x", "o" }, key, function()
+                require("nvim-treesitter-textobjects.move")[method](query, "textobjects")
+              end, {
+                buffer = buf,
+                desc = desc,
+                silent = true,
+              })
+            end
+          end
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("treesitter_textobjects", { clear = true }),
+        callback = function(ev)
+          attach(ev.buf)
+        end,
+      })
+      vim.tbl_map(attach, vim.api.nvim_list_bufs())
+    end,
+  },
+
+  {
+    "windwp/nvim-ts-autotag",
+    event = { "BufReadPre" },
+    opts = {},
   }
 }
 
