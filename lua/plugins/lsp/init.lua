3de99c503db14300243c114ea02fdc9bf1b8d6ac
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-28 15:26:03 +0800

    refactor: move flutter plugin to lsp dir

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,13 +1,17 @@
 return {
-  "neovim/nvim-lspconfig", -- enable LSP
-  import = "plugins.lsp.metals",
+  {
+    "neovim/nvim-lspconfig", -- enable LSP
 
-  config = function()
-    require "plugins.lsp.mason"
-    require "plugins.lsp.handlers".setup()
-  end,
-  dependencies = {
-    "mason-org/mason.nvim", -- auto manage lsp packages
-    "mason-org/mason-lspconfig.nvim", -- combine lspconfig with mason
-  }
+    config = function()
+      require "plugins.lsp.mason"
+      require "plugins.lsp.handlers".setup()
+    end,
+    dependencies = {
+      "mason-org/mason.nvim", -- auto manage lsp packages
+      "mason-org/mason-lspconfig.nvim", -- combine lspconfig with mason
+    },
+  },
+
+  { import = "plugins.lsp.metals"  },
+  { import = "plugins.lsp.flutter" },
 }
