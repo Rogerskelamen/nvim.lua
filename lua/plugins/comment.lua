0da5cc2a7515d7f3ffedbecb00dd8fe805b79526
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-28 10:54:16 +0800

    refactor: change config structure for Comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,35 +1,31 @@
 return {
   "numToStr/Comment.nvim",
-  event = "VeryLazy",
+  event = "BufReadPost",
 
-  config = function()
-    local status_ok, comment = pcall(require, "Comment")
-    if not status_ok then
-      return
-    end
-
-    comment.setup {
-      toggler = {
-        ---Line-comment toggle keymap
-        line = "<leader>c<leader>",
-        ---Block-comment toggle keymap
-        block = "<leader>cb",
-      },
-      opleader = {
-        ---Line-comment keymap
-        line = "<leader>c<leader>",
-        ---Block-comment keymap
-        block = "<leader>cb",
-      },
-      extra = {
-        ---Add comment on the line above
-        above = "<leader>cO",
-        ---Add comment on the line below
-        below = "<leader>co",
-        ---Add comment at the end of line
-        eol = "<leader>ca",
-      },
-    }
+  opts = {
+    toggler = {
+      ---Line-comment toggle keymap
+      line = "<leader>c<leader>",
+      ---Block-comment toggle keymap
+      block = "<leader>cb",
+    },
+    opleader = {
+      ---Line-comment keymap
+      line = "<leader>c<leader>",
+      ---Block-comment keymap
+      block = "<leader>cb",
+    },
+    extra = {
+      ---Add comment on the line above
+      above = "<leader>cO",
+      ---Add comment on the line below
+      below = "<leader>co",
+      ---Add comment at the end of line
+      eol = "<leader>ca",
+    },
+  },
+  config = function(_, opts)
+    require("Comment").setup(opts)
 
     local ft = require("Comment.ft")
     ft({"lex", "yacc"}, ft.get("c")) -- for lex/yacc
