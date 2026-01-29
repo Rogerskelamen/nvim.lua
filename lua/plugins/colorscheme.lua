4ed0f625bc43bb9bc91cf1cf776f9d191cb2756d
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-29 13:40:33 +0800

    feat: add rose-pine as new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -8,6 +8,11 @@ return {
     lazy = true,
   },
   {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    lazy = true,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     lazy = true
