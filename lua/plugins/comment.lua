fce907e4a20734ab0d7d3f4d6d0b3c5b608a5a59
Rogerskelamen <rogerskelamen@gmail.com>
2026-01-30 13:50:29 +0800

    feat: add todo-comments.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,33 +1,49 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPost", "BufNewFile" },
+  {
+    "numToStr/Comment.nvim",
+    event = { "BufReadPost", "BufNewFile" },
 
-  opts = {
-    toggler = {
-      ---Line-comment toggle keymap
-      line = "<leader>c<leader>",
-      ---Block-comment toggle keymap
-      block = "<leader>cb",
+    opts = {
+      toggler = {
+        ---Line-comment toggle keymap
+        line = "<leader>c<leader>",
+        ---Block-comment toggle keymap
+        block = "<leader>cb",
+      },
+      opleader = {
+        ---Line-comment keymap
+        line = "<leader>c<leader>",
+        ---Block-comment keymap
+        block = "<leader>cb",
+      },
+      extra = {
+        ---Add comment on the line above
+        above = "<leader>cO",
+        ---Add comment on the line below
+        below = "<leader>co",
+        ---Add comment at the end of line
+        eol = "<leader>ca",
+      },
     },
-    opleader = {
-      ---Line-comment keymap
-      line = "<leader>c<leader>",
-      ---Block-comment keymap
-      block = "<leader>cb",
-    },
-    extra = {
-      ---Add comment on the line above
-      above = "<leader>cO",
-      ---Add comment on the line below
-      below = "<leader>co",
-      ---Add comment at the end of line
-      eol = "<leader>ca",
-    },
+    config = function(_, opts)
+      require("Comment").setup(opts)
+
+      local ft = require("Comment.ft")
+      ft({"lex", "yacc"}, ft.get("c")) -- for lex/yacc
+    end,
   },
-  config = function(_, opts)
-    require("Comment").setup(opts)
 
-    local ft = require("Comment.ft")
-    ft({"lex", "yacc"}, ft.get("c")) -- for lex/yacc
-  end,
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      signs = false, -- show icons in the signs column
+      highlight = {
+        multiline = false,
+        keyword = "bg",
+        after = "",
+        exclude = {}, -- list of file types to exclude highlighting
+      }
+    }
+  }
 }
