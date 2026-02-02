98e8a18c16820b637bbcdbf22c6e1e7d32979267
Rogerskelamen <rogerskelamen@gmail.com>
2026-02-02 13:22:17 +0800

    style: change function declaration for utils.lua

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,10 +1,12 @@
 local M = {}
 
+local uv = vim.uv or vim.loop
+
 M.__IS_WIN = vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
 M.__IS_WT  = vim.env.WT_SESSION ~= nil
 
-M.is_darwin = function ()
-  return vim.uv.os_uname().sysname == "Darwin"
+function M.is_darwin ()
+  return uv.os_uname().sysname == "Darwin"
 end
 
 return M
