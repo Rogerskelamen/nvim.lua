df3afe520969400f7f797c78db45bfbd68a13ed9
Rogerskelamen <rogerskelamen@gmail.com>
2026-02-03 21:20:30 +0800

    refactor: reorgnize the plugins for markdown files

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,43 +1,44 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  ft = { "markdown" },
-  build = "cd app && npm install && git restore .",
-  dependencies = {
-    "dhruvasagar/vim-table-mode",
-    "MeanderingProgrammer/render-markdown.nvim",
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = { "markdown" },
+    build = "cd app && npm install && git restore .",
+    config = function ()
+      local g = vim.g
+      -- set to 1, nvim will open the preview window after entering the markdown buffer
+      g.mkdp_auto_start = 0
+      -- set to 1, the nvim will auto close current preview window when change
+      g.mkdp_auto_close = 0
+      -- set to 1, the MarkdownPreview command can be use for all files,
+      -- by default it can be use in markdown file
+      g.mkdp_command_for_global = 0
+    end
   },
+  {
+    "dhruvasagar/vim-table-mode",
+    ft = { "markdown" },
+    config = function ()
+      vim.cmd [[
+        function! s:isAtStartOfLine(mapping)
+        let text_before_cursor = getline('.')[0 : col('.')-1]
+        let mapping_pattern = '\V' . escape(a:mapping, '\')
+        let comment_pattern = '\V' . escape(substitute(&l:commentstring, '%s.*$', '', ''), '\')
+        return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
+        endfunction
 
-  config = function()
-    local g = vim.g
-
-    -- Markdown Preview --
-    -- set to 1, nvim will open the preview window after entering the markdown buffer
-    g.mkdp_auto_start = 0
-    -- set to 1, the nvim will auto close current preview window when change
-    g.mkdp_auto_close = 0
-    -- set to 1, the MarkdownPreview command can be use for all files,
-    -- by default it can be use in markdown file
-    g.mkdp_command_for_global = 0
-
-    -- Vim Table Mode
-    vim.cmd [[
-      function! s:isAtStartOfLine(mapping)
-      let text_before_cursor = getline('.')[0 : col('.')-1]
-      let mapping_pattern = '\V' . escape(a:mapping, '\')
-      let comment_pattern = '\V' . escape(substitute(&l:commentstring, '%s.*$', '', ''), '\')
-      return (text_before_cursor =~? '^' . ('\v(' . comment_pattern . '\v)?') . '\s*\v' . mapping_pattern . '\v$')
-      endfunction
-
-      inoreabbrev <expr> <bar><bar>
-      \ <SID>isAtStartOfLine('\|\|') ?
-      \ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
-      inoreabbrev <expr> __
-      \ <SID>isAtStartOfLine('__') ?
-      \ '<c-o>:silent! TableModeDisable<cr>' : '__'
-    ]]
-
-    -- opts for render-markdown.nvim
-    local render_opt = {
+        inoreabbrev <expr> <bar><bar>
+        \ <SID>isAtStartOfLine('\|\|') ?
+        \ '<c-o>:TableModeEnable<cr><bar><space><bar><left><left>' : '<bar><bar>'
+        inoreabbrev <expr> __
+        \ <SID>isAtStartOfLine('__') ?
+        \ '<c-o>:silent! TableModeDisable<cr>' : '__'
+      ]]
+    end
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown" },
+    opts = {
       sign = { enabled = false }, -- turn off the sign displayed in column
       completions = {
         lsp = { enabled = true },
@@ -140,12 +141,5 @@ return {
         }
       }
     }
-
-    local status_ok, render_md = pcall(require, 'render-markdown')
-    if not status_ok then
-      return
-    end
-
-    render_md.setup(render_opt)
-  end
+  }
 }
