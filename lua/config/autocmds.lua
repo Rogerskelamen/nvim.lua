16312493bff3f7b9224afbce04e5cbc509a1a49d
Rogerskelamen <rogerskelamen@gmail.com>
2026-02-19 20:26:53 +0800

    chore(autocmds): add autogroup to autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,6 +1,8 @@
 local api = vim.api
+local user_autogroup = api.nvim_create_augroup("UserAutoCmds", { clear = true })
 
 api.nvim_create_autocmd("FileType", {
+  group = user_autogroup,
   callback = function()
     -- remove 'o'，avoiding o/O insert comments automatically
     vim.opt_local.formatoptions:remove({ "o" })
@@ -9,6 +11,7 @@ api.nvim_create_autocmd("FileType", {
 
 -- Put cursor on the position where last time edited
 api.nvim_create_autocmd("BufReadPost", {
+  group = user_autogroup,
   callback = function()
     local mark = vim.api.nvim_buf_get_mark(0, '"')
     local lnum = mark[1]
